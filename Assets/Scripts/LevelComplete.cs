7b0bd285e1a40649715e7f3f42130a945e4f987f
Sebastien Schaeffler <sebastien.schaeffler@clearstream.com>
2018-08-05 13:18:43 +0200

    First code drop

---
+++ Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelComplete : MonoBehaviour {
+
+	public void goToNextLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody Rb;
+
+    public float ForwardForce = 2000f;
+    public float SideForce = 500f;
+
+    void FixedUpdate()
+    {
+        Rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            Rb.AddForce(SideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            Rb.AddForce(-SideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
