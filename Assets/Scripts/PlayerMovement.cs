d36bc10729e849b264179d0b5ca40b317605c22d
Sebastien Schaeffler <bidispot@gmail.com>
2018-10-07 11:47:41 +0200

    Add iOS and Android support

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -6,17 +7,59 @@ public class PlayerMovement : MonoBehaviour
 
     public float ForwardForce = 2000f;
     public float SideForce = 500f;
+    private Vector2 touchOrigin = -Vector2.one;
 
     void FixedUpdate()
     {
+        var left = 0;
+        var right = 0;
         Rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
 
+#if UNITY_STANDALONE || UNITY_WEBPLAYER //|| UNITY_EDITOR
         if (Input.GetKey("d"))
         {
-            Rb.AddForce(SideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            right = 1;
+            left = 0;
         }
 
         if (Input.GetKey("a"))
+        {
+            right = 0;
+            left = 1;
+        }
+#else
+        SideForce = 30f;
+        if (Input.touchCount > 0)
+        {
+            var myTouch = Input.touches[0];
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                touchOrigin = myTouch.position;
+            }
+            else if (myTouch.phase == TouchPhase.Moved && touchOrigin.x >= 0)
+            {
+                Vector2 touchEnd = myTouch.position;
+                var x = touchEnd.x - touchOrigin.x;
+
+                if (x < 0)
+                {
+                    left = 1;
+                    right = 0;
+                }
+                else
+                {
+                    left = 0;
+                    right = 1;
+                }
+            }
+        }
+#endif
+
+        if (right > 0)
+        {
+            Rb.AddForce(SideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if (left > 0)
         {
             Rb.AddForce(-SideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
--- README.md
+++ README.md
@@ -2,14 +2,20 @@
 
 # CubeCrash
 
-This is the first 3D mini-game I built with `Unity 2018` inspired by the great work of [Brackets](https://www.youtube.com/channel/UCYbK_tjZ2OrIZFBvU6CCMiA).
+This is the first 3D mini-game I built with `Unity 2018` inspired by the great work of [Brackeys](https://www.youtube.com/channel/UCYbK_tjZ2OrIZFBvU6CCMiA).
 
 This is an arcade game in which you're controlling a cube that is sliding on a quite narrow platform trying to avoid obstacles to reach the finish line.
 
+## Mobile version
+
+It's been for [Android] (https://github.com/bidispot/CubeCrash/blob/master/deliverables/CubeCrashAndroid.apk) and iOS (please contact me if you want to try it out since I cannot publish the iOS executable here)
+
+## Desktop version
+
 It's been built for [Windows](https://github.com/bidispot/CubeCrash/blob/master/deliverables/CubeCrash.zip) and [MacOs](https://github.com/bidispot/CubeCrash/tree/master/deliverables)
 
-Press `A` to go `Left`
+Press `a` to go `Left`
 
-Press `D` to go `Right`
+Press `d` to go `Right`
 
 ![alt Level 01](https://github.com/bidispot/CubeCrash/blob/master/CubeCrashScreenShot.png)
